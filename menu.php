a0a313f93b269e8520aafdb52e2726da729a58c3
seavor <jeremy@craftinc.co>
2014-07-10 00:01:17 -0700

    rest/menu php refactor done

--- menu.php
+++ menu.php
@@ -4,6 +4,14 @@ $pageClass = 'menu';
 include 'includes/meta.php';
 ?>
 
+<?php
+
+$data = json_decode(file_get_contents('http://eric-song.com:8000/restaurant_details?rid=' . $_GET['rid']), true);
+
+?>
+
+
+
 <!-- @TODO Escape GET Str -->
 <?php $_SESSION['rid'] = $_GET['rid']; ?>
 
@@ -18,28 +26,28 @@ include 'includes/meta.php';
 
 				<div id="menuHeader" class="restaurantListingItem">
 					<div class="topBox clearfix">
-						<h5>Restaurant Name</h5>
+						<h5><?php echo $data['name']; ?></h5>
 						<p class="distanceAway">0.3m</p>
 					</div>
 					<div class="bottomBox clearfix">
 						<div class="feeWrapper">
-							<p>Minimum Order: $10.00</p>
-							<p>Delivery Fee: $0.00</p>
+							<p>Minimum Order: $1x.00</p>
+							<p>Delivery Fee: $x.00</p>
 						</div>
 						<div class="appleRating">
 							<?php
-								$apple = 1;
-								switch ($apple) {
-									case 1: $applex = 0; $appley = 0; $applez = 1; break;
-									case 2: $applex = 0; $appley = 1; $applez = 1; break;
-									case 3: $applex = 1; $appley = 1; $applez = 1; break;
-									default: $applex = 0; $appley = 0; $applez = 0; break;
+								$rating = $data['rating'];
+								switch ($rating) {
+									case 1: $ratingx = 0; $ratingy = 0; $ratingz = 1; break;
+									case 2: $ratingx = 0; $ratingy = 1; $ratingz = 1; break;
+									case 3: $ratingx = 1; $ratingy = 1; $ratingz = 1; break;
+									default: $ratingx = 0; $ratingy = 0; $ratingz = 0; break;
 								}
 							?>
 							<div class="appleWrapper">
-								<img src="images/apple<?php echo $applex; ?>.png">
-								<img src="images/apple<?php echo $appley; ?>.png">
-								<img src="images/apple<?php echo $applez; ?>.png">
+								<img src="images/apple<?php echo $ratingx; ?>.png">
+								<img src="images/apple<?php echo $ratingy; ?>.png">
+								<img src="images/apple<?php echo $ratingz; ?>.png">
 							</div>
 						</div>
 					</div>
@@ -47,156 +55,34 @@ include 'includes/meta.php';
 
 				<div id='menuTip' class="healthTips">
 					<h4>Jay's Tips</h4>
-					<p>
-						This restaurant is loaded with fats and sugars, and as well as carbs
-						from the pancakes and toast. With restaurant like this, you want to hold
-						the butter and go as light as possible with the syrup on the pancakes.
-						If possible, ask for your eggs to be poached as opposed to fried or scrambled.
-					</p>
+					<p><?php echo $data['tip']; ?></p>
 				</div>
 
-				<div id="menuCategories">
-					<div class="catPanel">
-						<h3 id="catAppetizers">Appetizers</h3>
-						<div class="catItems">
-							<div class="healthTips">
-								<p>
-									Food like this has a lot of red-flag ingredients. When picking items from this
-									category, be careful to avoid this, that and the third. It’s best to do this
-									and another thing.
-								</p>
-							</div>
-							<div class="menuItem itemBest">
-								<h6>Farmer's Breakfast</h6>
-								<p class="itemPrice">$6.99</p>
-								<p class="itemDescription">
-									2 eggs (sunny-side up or scrambled), hash browns, whole-wheat biscuit and orange juice
-								</p>
-							</div>
-							<div class="menuItem itemGood">
-								<h6>Loaded Pancake Breakfast</h6>
-								<p class="itemPrice">$10.99</p>
-								<p class="itemDescription">
-									2 extra-thick buttermilk pancakes, served with 2 eggs (sunny-side up or scrambled),
-									buttered toast, coffee and orange juice
-								</p>
-							</div>
-							<div class="menuItem itemBetter">
-								<h6>Breakfast on the Go</h6>
-								<p class="itemPrice">$8.99</p>
-								<p class="itemDescription">
-									Bacon, egg and cheese on a whole-wheat biscuit, hash browns and coffee
-								</p>
+				<!-- Display each Category -->
+				<?php foreach ($data['menu'] as $value) { ?>
+					<div id="menuCategories">
+						<div class="catPanel">
+							<h3><?php echo $value['name'] ?></h3>
+							<div class="catItems">
+								<div class="healthTips">
+									<p><?php echo $value['tip'] ?></p>
+								</div>
+
+								<!-- Display each Category's Menu Item -->
+								<?php foreach ($value['children'] as $items) { ?>
+									<div class="menuItem itemBest" data-iid='<?php echo $items['id']; ?>'>
+										<h6><?php echo $items['name']; ?></h6>
+										<p class="itemPrice">$<?php echo $items['price']; ?></p>
+										<p class="itemDescription"><?php echo $items['descrip']; ?></p>
+									</div>
+								<?php } ?>
+
 							</div>
 						</div>
 					</div>
-					<div class="catPanel">
-						<h3 id="catEntrees">Entrees</h3>
-						<div class="catItems">
-							<div class="healthTips">
-								<p>
-									Food like this has a lot of red-flag ingredients. When picking items from this
-									category, be careful to avoid this, that and the third. It’s best to do this
-									and another thing.
-								</p>
-							</div>
-							<div class="menuItem itemBest">
-								<h6>Farmer's Breakfast</h6>
-								<p class="itemPrice">$6.99</p>
-								<p class="itemDescription">
-									2 eggs (sunny-side up or scrambled), hash browns, whole-wheat biscuit and orange juice
-								</p>
-							</div>
-							<div class="menuItem itemGood">
-								<h6>Loaded Pancake Breakfast</h6>
-								<p class="itemPrice">$10.99</p>
-								<p class="itemDescription">
-									2 extra-thick buttermilk pancakes, served with 2 eggs (sunny-side up or scrambled),
-									buttered toast, coffee and orange juice
-								</p>
-							</div>
-							<div class="menuItem itemBetter">
-								<h6>Breakfast on the Go</h6>
-								<p class="itemPrice">$8.99</p>
-								<p class="itemDescription">
-									Bacon, egg and cheese on a whole-wheat biscuit, hash browns and coffee
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="catPanel">
-						<h3 id="catSides">Sides</h3>
-						<div class="catItems">
-							<div class="healthTips">
-								<p>
-									Food like this has a lot of red-flag ingredients. When picking items from this
-									category, be careful to avoid this, that and the third. It’s best to do this
-									and another thing.
-								</p>
-							</div>
-							<div class="menuItem itemBest">
-								<h6>Farmer's Breakfast</h6>
-								<p class="itemPrice">$6.99</p>
-								<p class="itemDescription">
-									2 eggs (sunny-side up or scrambled), hash browns, whole-wheat biscuit and orange juice
-								</p>
-							</div>
-							<div class="menuItem itemGood">
-								<h6>Loaded Pancake Breakfast</h6>
-								<p class="itemPrice">$10.99</p>
-								<p class="itemDescription">
-									2 extra-thick buttermilk pancakes, served with 2 eggs (sunny-side up or scrambled),
-									buttered toast, coffee and orange juice
-								</p>
-							</div>
-							<div class="menuItem itemBetter">
-								<h6>Breakfast on the Go</h6>
-								<p class="itemPrice">$8.99</p>
-								<p class="itemDescription">
-									Bacon, egg and cheese on a whole-wheat biscuit, hash browns and coffee
-								</p>
-							</div>
-						</div>
-					</div>
-					<div class="catPanel">
-						<h3 id="catDrinks">Drinks</h3>
-						<div class="catItems">
-							<div class="healthTips">
-								<p>
-									Food like this has a lot of red-flag ingredients. When picking items from this
-									category, be careful to avoid this, that and the third. It’s best to do this
-									and another thing.
-								</p>
-							</div>
-							<div class="menuItem itemBest">
-								<h6>Farmer's Breakfast</h6>
-								<p class="itemPrice">$6.99</p>
-								<p class="itemDescription">
-									2 eggs (sunny-side up or scrambled), hash browns, whole-wheat biscuit and orange juice
-								</p>
-							</div>
-							<div class="menuItem itemGood">
-								<h6>Loaded Pancake Breakfast</h6>
-								<p class="itemPrice">$10.99</p>
-								<p class="itemDescription">
-									2 extra-thick buttermilk pancakes, served with 2 eggs (sunny-side up or scrambled),
-									buttered toast, coffee and orange juice
-								</p>
-							</div>
-							<div class="menuItem itemBetter">
-								<h6>Breakfast on the Go</h6>
-								<p class="itemPrice">$8.99</p>
-								<p class="itemDescription">
-									Bacon, egg and cheese on a whole-wheat biscuit, hash browns and coffee
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
+				<?php } ?>
 
 				<p class="hidden" id="tray"></p>
-
-
 
 				<!-- Show only once item has been added to order -->
 				<?php
