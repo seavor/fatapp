3d45e9a24fd0ac17f9275ac7e8fca885e8cd4ceb
seavor <jeremy@craftinc.co>
2014-07-15 23:08:24 -0500

    angular setup

--- includes/meta.php
+++ includes/meta.php
@@ -1,24 +1,3 @@
-
-
-<!-- ******************************************* -->
-	<?php
-
-		include 'includes/functions.php';
-
-		$_SESSION['previous'] = basename($_SERVER['HTTP_REFERER']);
-		$userLoggedIn = $_SESSION['userLoggedIn'] = false; # No Users for this Version
-
-		$siteRoot = "http://localhost:8888/";
-		$dbRoot = 'http://162.243.27.144:8000/';
-
-	?>
-
-	<script type="text/javascript">
-		var siteRoot = "<?php echo $siteRoot; ?>";
-		var userLoggedIn = "<?php echo $userLoggedIn; ?>";
-		var pageClass = "<?php echo $pageClass; ?>";
-	</script>
-<!-- ******************************************* -->
 <html>
 
 	<head>
@@ -26,12 +5,13 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<meta name="description" content="Eat Healthy, Be Healthy">
-		<title>Fat App | <?php echo $pageTitle; ?></title>
-		<link rel="icon" href="<?php echo $siteRoot; ?>images/header/favicon.png" sizes="16x16" type="image/png">
-		<link rel="stylesheet" type="text/css" href="<?php echo $siteRoot; ?>css/normalize.min.css" />
-		<link rel="stylesheet" type="text/css" href="<?php echo $siteRoot; ?>css/style.css" />
+		<title>Fat App</title>
+		<link rel="icon" href="images/header/favicon.png" sizes="16x16" type="image/png">
+		<link rel="stylesheet" type="text/css" href="css/normalize.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
 		<script type="text/javascript" src="<?php echo $siteRoot; ?>js/jquery-1.8.3.min.js"></script>
 		<script type="text/javascript" src="<?php echo $siteRoot; ?>js/jquery.js"></script>
+		<script type="text/javascript" src="<?php echo $siteRoot; ?>js/pol.js"></script>
 
 			<!--[if lt IE 9]>
 				<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -44,5 +24,39 @@
 			<![endif]-->
 
 	</head>
-			
-	<body class="<?php echo $pageClass; ?>">
+
+<body>
+
+	<div id="appHeader">
+
+		<div id="homeButton"></div>
+
+		<div id="pageHeading">
+			<h1><?php echo $pageTitle; ?></h1>
+		</div>
+
+		<div id="navButton" data-button="navBar">
+			<img src="images/menu.png">
+		</div>
+
+		<div id="navItems" class="slideMenu" data-display="navBar">
+			<ul>
+				<li>
+					<a href="account.php">My Account</a>
+				</li>
+			</ul>
+		</div>
+
+	</div>
+
+
+
+
+	<footer>
+
+
+
+	</footer>
+
+</body>
+</html>
