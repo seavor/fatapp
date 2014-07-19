f2b253f412fcc3016f40abd4618343afa497bf31
seavor <jeremy@craftinc.co>
2014-07-18 22:27:46 -0500

    search page finished

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -52,7 +52,7 @@ app.config(['$routeProvider',
 
 		// Initialize Modal Variables
 		$scope.displayModal = false;
-		$scope.url = '';
+		// $scope.url = '';
 
 		$scope.popupModal = function(popup){
 			$scope.url = popup;
@@ -76,9 +76,9 @@ app.config(['$routeProvider',
 	app.controller('SavedAddressesCtrl', function($scope, $http, $location, ControllerStorage){
 		$scope.addressList = [
 			// @TODO ajax call for address info from (aid)
-		    {"aid":"001", "addrName":"Home", "addressLine":"392 Broadway", "city":"New York", "state":"NY", "zip":"10013"}, 
-		    {"aid":"002", "addrName":"Work", "addressLine":"920 Broadway", "city":"New York", "state":"NY", "zip":"10010"}, 
-		    {"aid":"003", "addrName":"School", "addressLine":"1480 Broadway", "city":"New York", "state":"NY", "zip":"10036"}
+		    {"aid":"001", "addressName":"Home", "addressLine":"392 Broadway", "city":"New York", "state":"NY", "zipcode":"10013"}, 
+		    {"aid":"002", "addressName":"Work", "addressLine":"920 Broadway", "city":"New York", "state":"NY", "zipcode":"10010"}, 
+		    {"aid":"003", "addressName":"School", "addressLine":"1480 Broadway", "city":"New York", "state":"NY", "zipcode":"10036"}
 		]
 
 		$scope.storage = ControllerStorage;
@@ -93,11 +93,54 @@ app.config(['$routeProvider',
 
 	});
 
-	app.controller('RestFiltersCtrl', function($scope, $http, $location, ControllerStorage){
+	app.controller('RestFilterCtrl', function($rootScope, $scope, ControllerStorage){
 
 		$scope.storage = ControllerStorage;
 
-		
+		$scope.filterForm = $scope.storage.filterForm;
+
+
+		$scope.filterControl = function(filter){
+
+			if ( this.filterForm.all ) {
+				if( filter !== 'all') {
+					this.filterForm.all = false;
+				}
+			}
+
+			if ( (this.filterForm.brunch && this.filterForm.lunch && this.filterForm.dinner) || (filter == 'all') ) {
+				this.filterForm.brunch = false;
+				this.filterForm.lunch = false;
+				this.filterForm.dinner = false;
+				this.filterForm.all = true;
+			}
+
+		};
+
+		$scope.filterSelection = function(){
+			if ($scope.filterForm.all) {
+				$scope.storage.filterForm.brunch = false;
+				$scope.storage.filterForm.lunch = false;
+				$scope.storage.filterForm.dinner = false;
+			};
+
+			var filterDisp = 'Filter: ';
+			for(var filter in $scope.filterForm ) {
+				if( $scope.filterForm[ filter ] ) {
+					filterDisp += filter.charAt(0).toUpperCase() + filter.slice(1);
+					// Append 'Menus' if filter is All
+					if (filter == 'all') { filterDisp += ' Menus  '; }
+					else { filterDisp += ', '; }
+				}	
+			}
+
+			// Remove Trailing Comma
+			filterDisp = filterDisp.slice(0, -2);
+			$scope.storage.filterDisplay = filterDisp;
+
+			$scope.closeModal();
+
+		};
 
 	});
 
@@ -109,18 +152,34 @@ app.config(['$routeProvider',
 
 		$scope.storage = ControllerStorage;
 
-		// init variables for address form
-		$scope.addrForm = {};
-		$scope.addrForm.addressLine = '';
-		$scope.addrForm.city = '';
-		$scope.addrForm.zipcode = '';
-		$scope.addrForm.nickname = '';
+			// // init variables for address form
+			// $scope.addrForm = {};
+			// $scope.addrForm.addressLine = '';
+			// $scope.addrForm.city = '';
+			// $scope.addrForm.zipcode = '';
+			// $scope.addrForm.nickname = '';
+			// $scope.addressDisplay = '';
 
-		$scope.addressDisplay ='';
+			// Initialize Filter Checkbox
+			$scope.filterForm = { "all" : true };
 
-		$scope.storage.findRestButtonState = false;
+			// Initialize Filter & Address Storage Space
+			$scope.storage.filterForm = $scope.filterForm;
+			// $scope.storage.addressObj = {};
+			
+			// Initialize Filter Display
+			$scope.storage.filterDisplay = 'Filter: All Menus';
+
+			// Keep Filtered Display Updated
+			$scope.$watch('storage.filterDisplay', function() {
+				$scope.filterDisplay = $scope.storage.filterDisplay;
+			});
+
+			// Reveal 'Find Restaurants' button upon Address Selection
+			$scope.storage.findRestButtonState = false;
 
 		$scope.displayRestButton = function(){
+			// Change this to check against storage of search address
 			var state = $scope.storage.findRestButtonState;
 			return state;
 		};
@@ -137,14 +196,10 @@ app.config(['$routeProvider',
 		$scope.chooseAddress = function(aid){
 			$scope.storage.addressList.forEach(function(addr) {
 			    if (aid == addr.aid) {
-			    	$scope.storage.addrLine = addr.addressLine;
-					$scope.storage.city = addr.city;
-					$scope.storage.zipcode = addr.zip;
-					$scope.storage.nickname = addr.addrName;
-					$scope.closeModal();
-					$scope.addressDisplay = $scope.storage.addrLine + ', ' + $scope.storage.city + ', ' + $scope.storage.zipcode;
+			    	$scope.storage.addressObj = addr;
+					$scope.addressDisplay = $scope.storage.addressObj.addressLine + ', ' + $scope.storage.addressObj.city + ', ' + $scope.storage.addressObj.zipcode;
 					$scope.storage.findRestButtonState = true;
-
+					$scope.closeModal();
 				}
 			});
 			// @TODO
@@ -153,14 +208,11 @@ app.config(['$routeProvider',
 
 		// Store New Address
 		$scope.storeAddress = function() {
-			$scope.storage.addrLine = $scope.addrForm.addressLine;
-			$scope.storage.city = $scope.addrForm.city;
-			$scope.storage.zipcode = $scope.addrForm.zipcode;
-			$scope.storage.nickname = $scope.addrForm.nickname;
+			$scope.storage.addressObj = $scope.addrForm;
 			// TODO: store if user logged in
-			$scope.closeModal();
-			$scope.addressDisplay = $scope.storage.addrLine + ', ' + $scope.storage.city + ', ' + $scope.storage.zipcode;
+			$scope.addressDisplay = $scope.storage.addressObj.addressLine + ', ' + $scope.storage.addressObj.city + ', ' + $scope.storage.addressObj.zipcode;
 			$scope.storage.findRestButtonState = true;
+			$scope.closeModal();
 		};
 
 
@@ -168,14 +220,7 @@ app.config(['$routeProvider',
 		$scope.saveAddress = function(){
 
 		};
-
 		
-
-
-
-
-
-
 
 		// Find restaurants
 		$scope.findRestaurants = function(){
@@ -200,8 +245,6 @@ app.config(['$routeProvider',
 			$location.path('/restaurants');
 		};
 
-
-
 	});
 
 
@@ -220,6 +263,17 @@ app.config(['$routeProvider',
 		$scope.cuisines = 'American';
 
 		$scope.serviceDemo = $scope.storage.addrLine;
+
+		$scope.filterDisplay = $scope.storage.filterDisplay;
+
+		// Keep Filtered Field Updated
+		$scope.$watch('storage.filterDisplay', function() {
+			$scope.filterDisplay = $scope.storage.filterDisplay;
+		});
+
+		$scope.ratingx = 1;
+		$scope.ratingy = 1;
+		$scope.ratingz = 0;
 
 
 		
