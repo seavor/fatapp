699099f90987e3667eee47e309566bedd24e823a
seavor <jeremy@craftinc.co>
2014-07-30 13:08:49 -0500

    minor changes

--- webapp/conditions.js
+++ webapp/conditions.js
@@ -1,52 +1,45 @@
-Restaurants [
-	{
-		id: restId (int)
-		filters: [Lunch, Dinner] (array to string)
-		rating: 2 (int)
-		tip: "blah blah" (str)
-		activated: 0 (bit)
-		changed: 0 (bit)
-	}
-]
+* Home Page (search) *
 
-Categories [
-	{
-		id: autoInc (int)
-		rest_id: restId (int) <= linked to Restaurant.id
-		name: "Entrees" (str)
-		tip: "blah blah" (str)
-	}
-]
+	- Guest
 
-Items [
-	{
-		id: itemId (int)
-		cat_id: (int) <= linked to Categories.id
-		rating: 2 (int)
-		options: [optionId, optionId] (array to string)
-		activated: 0 (bit)
-	}
-]
+	- LoggedIn
 
-Calls Needed
-options [
-		dressingType  {
-			Ranch
-		}
-]
-
-katz: "[19026423: [32911722], 32911739: [32911743, 32911748], 19035436: [19035437, 19035438], 19035440: [19035441, 19035442, 19035443]]"
+	- Address Not Valid
 
 
 
+* Restaurant Listing *
 
-options: [choiceId1, choiceId2, ...]
+	- New Address Search
+
+	- Repeat Search (close/open check)
+
+	- Ajax menu-call if New Restaurant is clicked
 
 
-optionCatID []
-	0: choiceID
-	1: choiceID
-}
+
+* Menu Page *
+
+	- Accordion Menu Animation
+
+	- View Menu Item
+
+	- Continue to Review Order page once cart != empty
 
 
-category -> item -> option Category -> choice
+
+* Review Page *
+
+	- Edit/Remove Item
+
+	- Proceed to Checkout if Restaurant Minimum is met
+
+
+
+* Checkout Page *
+
+	- Show "Send Me Food!" button once all fields are filled
+
+	- Validate Form Fields before processing order
+
+	- Route to Review Page upon successful order
